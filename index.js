7124a83f55f906274f8c38f368996aae30be2b27
Divna Petkovic <didimos88@hotmail.com>
2021-03-16 18:14:40 +0100

    primitive types.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // This is my first js code!
 console.log("Hello world");
 
-let name = 'Mosh';
+let name = 'Mosh'; //string literal
 console.log(name);
 
 // Variables cannot be reserved keywords
@@ -17,3 +17,15 @@ console.log(FirstName);
 let firstName = 'Mosh';
 let lastName = 'HAMEDANI';
 console.log(firstName, lastName);
+
+//Constant:
+const interestRate = 0.3;
+console.log(interestRate);
+
+// Primitive types:
+// string, number, boolean, undefined, null
+
+let age = 30; // number literal
+
+let isApproved = false; // boolean literal
+let selectedColor = null;
