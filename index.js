06988cf014066c53b6b7f4ae81acb9197c27e4de
Divna Petkovic <didimos88@hotmail.com>
2021-03-16 18:05:39 +0100

    variables.

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 // This is my first js code!
 console.log("Hello world");
+
+let name = 'Mosh';
+console.log(name);
+
+// Variables cannot be reserved keywords
+// they should be meaningful
+// cannot start with a number
+// cannot contain a space or hyphen
+// are case-sensitive
+
+
+let FirstName
+console.log(FirstName);
+
+let firstName = 'Mosh';
+let lastName = 'HAMEDANI';
+console.log(firstName, lastName);
