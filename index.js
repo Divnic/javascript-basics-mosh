a6a64cc92941de5e6f78c764156ec43af437706c
Divna Petkovic <didimos88@hotmail.com>
2021-03-17 17:45:38 +0100

    assignment operators.

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
+// Assignement operators
+
 let x = 10;
-let y = 3;
 
-// console.log(x+y);
-// console.log(x-y);
-// console.log(x*y);
-// console.log(x/y);
-// console.log(x % y); remainder of division
-// console.log(x ** y); exponentiation
+x++; //equivalent to x=x+1;
 
-// Increment (++)
-// console.log(++x); increments immediately
-// console.log(x++); first shows x, incrementation hidden until you print x
+x += 5; //equivalent to x = x+5;
 
-// Decrement (--)
-console.log(x--);
+x *=3; // equivalent to x=x*3;
+
 console.log(x);
