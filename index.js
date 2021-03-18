eba5a1e061049e68525771ca271a23f1dfe40997
Divna Petkovic <didimos88@hotmail.com>
2021-03-18 18:55:25 +0100

    logical operators.

--- index.js
+++ index.js
@@ -1,9 +1,38 @@
+// Logical AND (&&)
+// returns TRUE if both operands are true
 
-//If a customer has more than 100 points, 
-//they are a 'gold' customer, otherwise,
-//they are a silver customer.
+// console.log(true && true);
 
-let points = 90;
-let type = points > 100 ? 'gold' : 'silver';
+let highIncome = true;
+let goodCreditScore = true;
+let eligibleForLoan = highIncome && goodCreditScore;
 
-console.log(type);
+console.log('Eligible', eligibleForLoan);
+
+// Logical OR (||)
+// returns TRUE if one of the operands is true
+
+let cheese = true;
+let dessert = false;
+let afterMeal = cheese || dessert;
+
+console.log(afterMeal);
+
+// not (!)
+let applicationRefused = !eligibleForLoan;
+
+console.log('Application Refused', applicationRefused);
+
+
+// Logical operators with Non-booleans ()
+
+// Falsy values: undefined, null, 0, (boolean) false,'' (empty string), NaN (not a number)
+
+// Anything not Falsy -> Truthy
+
+let userColor = 'red';
+let defaultColor = 'blue';
+let currentColor = userColor || defaultColor;
+
+console.log(currentColor);
+
