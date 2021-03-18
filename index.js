601bf28d2d34b2cf1b3f3f780da5e855a5f0f248
Divna Petkovic <didimos88@hotmail.com>
2021-03-18 19:40:14 +0100

    swapping variables.

--- index.js
+++ index.js
@@ -1,29 +1,13 @@
-// decimal to binary conversion
+// swapping variables
 
-// 1 = 00000001
-// 2 = 00000010
-// 3 = 00000011   R =  (result of 1 OR 2) 
-// 0 = 00000000  R =  (result of 1 AND 2) 
+let a = 'red';
+let b = 'blue';
 
-// console.log(1 | 2); // | bitwise OR
-// console.log(1 & 2); // | bitwise AND
 
-// Imagine you wanna implement an access-controlled system
-// user can have these permissions:
-// read, write, execute
-// 00000100 = 4
-// 00000110
-// 00000111
+let c = a;
+a = b;
+b = c;
 
-const readPermission = '4'
-const writePermission = '2'
-const executePermission = '1'
+console.log(a);
+console.log(b);
 
-let myPermission = 0;
-myPermission = myPermission | readPermission | writePermission;
-// console.log(myPermission);
-
-let message = 
-(myPermission & readPermission) ? 'yes' : 'no';
-
-console.log(message);
