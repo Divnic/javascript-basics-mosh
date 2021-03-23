0eabe8cbdde9f985e6d3fb8caf36dc8c88aa27e6
Divna Petkovic <didimos88@hotmail.com>
2021-03-23 16:45:03 +0100

    for, while, do-while, for-in, for-of loops.

--- index.js
+++ index.js
@@ -1,22 +1,51 @@
-let role = 'guest';
+// for (initialExpression; condition; incrementExpression)
+//statement
 
-//switch (variable)
+ for (let i=1; i<=5; i++) {
+ console.log('Hello world', i);
+ }
 
-switch (role) {
-    case 'guest':
-        console.log('Guest User');
-        break;
-
-    case 'moderator':
-        console.log('Moderator User');
-        break; 
-    
-    default:
-        console.log('Unknown User');
+for (let i=1; i<=5; i++) {
+    if (i % 2 !==0)
+console.log(i);
 }
 
-// alternatively
+//while
 
-if (role === 'guest') console.log('Guest');
-else if (role === 'moderator') console.log('Moderator');
-else console.log('Unknown');
+// let i = 0;
+// while (i <= 5) {
+//     if (i % 2 !==0)
+//     console.log(i);
+//     i++;  
+// }
+
+//do... while 
+
+let i = 0;
+do {
+    if (i % 2 !==0)
+    console.log(i);
+    i++;    
+} while (i<=5);
+
+
+// for in loop - for iterating over the properties of an object
+
+const person = {
+    name: 'Mosh',
+    age: 30
+};
+
+for (let key in person)
+console.log(key, person[key]);
+
+const colors = ['red', 'green', 'blue'];
+
+// for (let index in colors)
+// console.log(index, colors[index]);
+
+// for... of loop - ideal for iterating over elements or items in arrays (new!)
+
+for (let color of colors)
+console.log(color);
+
