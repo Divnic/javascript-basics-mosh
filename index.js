e6a41a2980b9d63af61e3fb431b624e4ca360fde
Divna Petkovic <didimos88@hotmail.com>
2021-03-23 13:45:22 +0100

    if else condition syntax.

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
-// swapping variables
+// Hour
+// If hour is between 6am and 12pm : Good morning!
+// If hour is between 12pm and 6pm : Good afternoon!
+// Otherwise : Good evening!
 
-let a = 'red';
-let b = 'blue';
+// if (condition) {
+
+// statement
+    
+// }
+
+// else if (anotherCondition) {
+
+// }
+ 
+// else 
+//  statement
 
 
-let c = a;
-a = b;
-b = c;
+let hour = 20;
 
-console.log(a);
-console.log(b);
-
+if (hour >= 6 && hour < 12) 
+console.log('Good morning!');
+else if (hour >= 12 && hour < 18) 
+    console.log('Good afternoon!');
+else
+console.log('Good evening!');
