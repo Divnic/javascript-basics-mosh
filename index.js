69db9ead0ea554216aa5f08fe2ed0fddd22a309c
Divna Petkovic <didimos88@hotmail.com>
2021-03-23 13:52:15 +0100

    switch case conditionals syntax.

--- index.js
+++ index.js
@@ -1,27 +1,22 @@
-// Hour
-// If hour is between 6am and 12pm : Good morning!
-// If hour is between 12pm and 6pm : Good afternoon!
-// Otherwise : Good evening!
+let role = 'guest';
 
-// if (condition) {
+//switch (variable)
 
-// statement
+switch (role) {
+    case 'guest':
+        console.log('Guest User');
+        break;
+
+    case 'moderator':
+        console.log('Moderator User');
+        break; 
     
-// }
+    default:
+        console.log('Unknown User');
+}
 
-// else if (anotherCondition) {
+// alternatively
 
-// }
- 
-// else 
-//  statement
-
-
-let hour = 20;
-
-if (hour >= 6 && hour < 12) 
-console.log('Good morning!');
-else if (hour >= 12 && hour < 18) 
-    console.log('Good afternoon!');
-else
-console.log('Good evening!');
+if (role === 'guest') console.log('Guest');
+else if (role === 'moderator') console.log('Moderator');
+else console.log('Unknown');
