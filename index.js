eb6bb0655ae6dd5139c5e998e33964e059caf7fd
Divna Petkovic <didimos88@hotmail.com>
2021-03-26 17:01:18 +0100

    Exos 3 to 11 - reusable code.

--- index.js
+++ index.js
@@ -65,28 +65,239 @@
 // console.log(max(a,b))
 
 
-function max(a,b) {
-    return (a > b) ? a : b;
+//////////////////////////////////////
+// Exercise 3 : fizzBuzz
+
+
+const output = fizzBuzz(false);
+console.log(output);
+
+function fizzBuzz(input){
+    if (typeof input !== 'number')
+    return NaN; //'Not a number'
+
+    if ((input % 3 === 0) && (input % 5 === 0)) 
+    return 'Fizzbuzz';
+
+    if (input % 3 === 0) 
+    return 'Fizz';
+
+    if (input % 5 === 0) 
+    return 'Buzz';
+    
+    return input;
 }
 
-let number = max(17,18);
-console.log(number);
+
+//////////////////////////////////////
+// Exercice 4 : Demerit points
+
+// Speed limit = 70
+// for every 5 km above the speed limit = 1 point
+// more than 12 points - 'License suspended'
+// Math.floor(1.3)
 
 
-// Exercice 2: Implement function that returns true if the image is landscape
+checkSpeed(129);
 
-// function isLandscape(width, height) {
-//     if (width > height) 
-// return true;
-// return false; 
+
+
+function checkSpeed(speed) {
+    const speedLimit = 70;
+    const kmPerPoint = 5;
+
+    if (speed < speedLimit + kmPerPoint)
+    console.log('Ok');
+    else  {
+       const points =  Math.floor((speed - speedLimit) / kmPerPoint);
+
+       if (points >= 12)
+       console.log('License suspended');
+       else
+       console.log('Points', points);
+    }
+}
+
+
+
+// Tip to make the code cleaner - remove else block
+
+//function checkSpeed(speed) {
+//     const speedLimit = 70;
+//     const kmPerPoint = 5;
+
+//  if (speed < speedLimit + kmPerPoint{
+//     console.log('Ok');
+//     return;
 // }
-// no need to explicitly return true false
+//  
+//  const points =  Math.floor((speed - speedLimit) / kmPerPoint);
 
-function isLandscape(width, height) {
-    return (width > height); 
+//  if (points >= 12)
+//      console.log('License suspended');
+//  else
+//      console.log('Points', points);
+//     
+// }
+
+
+
+//////////////////////////////////////
+// Exercise 5 : even and odd numbers
+
+showNumbers(10);
+
+function showNumbers(limit) {
+    for (let i=0; i<=limit; i++) {
+    if (i % 2 === 0)
+    console.log(i, 'EVEN');
+    else
+    console.log(i, 'ODD');
+     }
 }
 
-console.log(isLandscape(800,600));
+// alternatively
+
+showNumbers(10);
+
+function showNumbers(limit) {
+    for (let i=0; i<=limit; i++) {
+const message = (i % 2 === 0) ? 'EVEN' : 'ODD'
+console.log(i, message);  
+     }
+}
+
+//////////////////////////////////////
+// Exercise 6 : counting truthy values
+
+const array = [1,2, "zouzou",3, undefined]
+
+function countTruthy(array) {
+    let count = 0;
+    for (let value of array)
+        if (value)
+            count++;
+    return count
+}
+
+console.log(countTruthy(array));
+
+//////////////////////////////////////
+// Exo 7 - display only string properties
+
+const movie = {
+title: 'a',
+releaseYear: 2018,
+rating: 4.5,
+director: 'b'
+};
+
+showProperties(movie);
+
+function showProperties(obj) {
+    for (let key in obj)
+      if (typeof obj[key] === 'string')
+      console.log(key, obj[key]);
+}
+
+//////////////////////////////////////
+// Exo 8 - sum of multiples of 3 and 5
+
+// Multiples of 3: 3, 6, 9
+// Multiples of 5: 5, 10
+
+function sum(limit) {
+    let sum = 0; //initialization
+
+    for (let i = 0; i<= limit; i++) // actual logic
+    if (i % 3 === 0 || i % 5 === 0)
+        sum +=i;
+
+    return sum; // return value
+
+}
+
+console.log(sum(10))
+
+//////////////////////////////////////
+// EXO 9 - grades
+
+const marks = [90, 90, 90];
+
+// 0-59: F
+// 60-69: D
+// 70-79: C
+// 80-89: B
+// 90-100: A
+
+console.log(calculateGrade(marks));
+
+function calculateGrade(marks) {
+  const average = calculateAverage(marks);
+
+    if (average < 60) return 'F';
+    if (average < 70) return 'D';
+    if (average < 80) return 'C';
+    if (average < 90) return 'B';
+    return 'A';
+
+}
+
+function calculateAverage(array) {
+    let sum = 0;
+
+    for (let value of array)
+        sum+= value;
+    return sum / array.length;
+} // generic function for averaging
+
+//////////////////////////////////////
+// Exo 10 nested loop
+
+showStars(5);
+
+function showStars(rows) {
+    for (let row = 1; row<= rows; row++) {
+        let pattern =  '';
+        for (let i = 0; i < row; i ++)
+        pattern  += '*';
+        console.log(pattern);
+    }
+}
 
 
 
+//////////////////////////////////////
+// Exo 11
+
+showPrimes(20);
+
+function showPrimes(limit) {
+    for (let number = 2; number <= limit; number++) {
+        // 2 - current (i)
+
+        let isPrime = true;
+        for (let factor = 2; factor < number; factor++) {
+            if (number % factor === 0) {
+                isPrime = false;
+                break;
+            }
+        } 
+      if (isPrime)  console.log(number);
+    }   
+}
+
+// and if we break it in smaller functions
+
+function showPrimes(limit) {
+    for (let number = 2; number <= limit; number++) 
+        // 2 - current (i)
+      if (isPrime(number))  console.log(number); 
+}
+
+function isPrime(number) {
+    for (let factor = 2; factor < number; factor++) 
+        if (number % factor === 0) 
+            return false;
+    return true;
+}
